e70facbe2473cf225ce02108f435935056239fb0
shreyasaha-dev <shreyasaha562@gmail.com>
2024-02-15 08:55:47 +0530

    dynamic the year

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import About from "../component/About";
 import "../style/Home.css";
+import moment from "moment";
 const Home = () => {
   const navigate = useNavigate();
   return (
     <>
       <div className="total-home-section">
         <div className="mid-section">
-          <h2>Collection of 2023</h2>
+          <h2>Collection of {moment().format("YYYY")}</h2>
           <h1>NEW ARRIVALS</h1>
           <button
             onClick={() => {
